3c06db8433b9ff533b94d5d39805c27cf259cd80
Whiffy <dbene77@gmail.com>
2022-01-31 14:10:17 -0500

    boids now appear on the screen

--- src/boid.rs
+++ src/boid.rs
@@ -1,25 +1,34 @@
-use nalgebra::{norm, Vector2};
+use nalgebra as na;
+use nalgebra::Vector2;
+use raylib::prelude::*;
 
 pub struct Boid {
     pub position: [f64; 2],
-    pub vector: Vector2,
+    pub vector: Vector2<f64>,
     pub view_distance: i32,
-    pub colour: [f32; 4],
+    pub colour: Color,
     pub num: u8,
 }
 
 impl Boid {
     pub fn accumulate_forces(&mut self, boids: &Vec<Boid>) {
         let align = self.alignment(boids);
-        let cohes = self.cohesion(boids);
+        //let cohes = self.cohesion(boids);
         let sepa = self.separation(boids);
-        self.vector[0] += align[0] + cohes[0] + sepa[0];
-        self.vector[1] += align[1] + cohes[1] + sepa[1];
-        self.vector = vm::vec2_normalized(self.vector);
+        //TODO: sum vector components
+        self.vector = na::base::Matrix::normalize(&self.vector);
     }
-    fn alignment(&mut self, boids: &Vec<Boid>) -> vm::Vector2<f64> {
+    pub fn render(&self, d: &mut RaylibDrawHandle, window: [i32; 2]) {
+        d.draw_circle(
+            self.position[0] as i32,
+            self.position[1] as i32,
+            5.0,
+            self.colour,
+        );
+    }
+    fn alignment(&mut self, boids: &Vec<Boid>) -> Vector2<f64> {
         let mut neighbour_count = 0.0;
-        let mut out: vm::Vector2<f64> = [0.0, 0.0];
+        let mut out: Vector2<f64> = Vector2::new(0.0, 0.0);
         for b in boids {
             if b.num != self.num {
                 if self.distance_from(b) <= self.view_distance as f64 {
@@ -32,34 +41,37 @@ impl Boid {
         if neighbour_count >= 0.0 {
             out[0] /= neighbour_count;
             out[1] /= neighbour_count;
-            return vm::vec2_normalized(out);
+            return na::base::Matrix::normalize(&out);
         } else {
-            return [0.0, 0.0];
+            return Vector2::new(0.0, 0.0);
         }
     }
-    fn cohesion(&mut self, boids: &Vec<Boid>) -> vm::Vector2<f64> {
+    //fn cohesion(&mut self, boids: &Vec<Boid>) -> Vector2<f64> {
+    //    let mut neighbour_count = 0.0;
+    //    let mut out = [0.0, 0.0];
+    //    for b in boids {
+    //        if b.num != self.num {
+    //            if self.distance_from(b) <= self.view_distance as f64 {
+    //                out[0] += b.position[0];
+    //                out[1] += b.position[1];
+    //                neighbour_count += 1.0;
+    //            }
+    //        }
+    //    }
+    //    if neighbour_count >= 0.0 {
+    //        out[0] /= neighbour_count;
+    //        out[1] /= neighbour_count;
+    //        return na::base::Matrix::normalize(&Vector2::new(
+    //            out[0] - self.vector[0],
+    //            out[1] - self.vector[1],
+    //        ));
+    //    } else {
+    //        return Vector2::new(0.0, 0.0);
+    //    }
+    //}
+    fn separation(&mut self, boids: &Vec<Boid>) -> Vector2<f64> {
         let mut neighbour_count = 0.0;
-        let mut out = [0.0, 0.0];
-        for b in boids {
-            if b.num != self.num {
-                if self.distance_from(b) <= self.view_distance as f64 {
-                    out[0] += b.position[0];
-                    out[1] += b.position[1];
-                    neighbour_count += 1.0;
-                }
-            }
-        }
-        if neighbour_count >= 0.0 {
-            out[0] /= neighbour_count;
-            out[1] /= neighbour_count;
-            return vm::vec2_normalized([out[0] - self.vector[0], out[1] - self.vector[1]]);
-        } else {
-            return [0.0, 0.0];
-        }
-    }
-    fn separation(&mut self, boids: &Vec<Boid>) -> vm::Vector2<f64> {
-        let mut neighbour_count = 0.0;
-        let mut out = [0.0, 0.0];
+        let mut out = Vector2::new(0.0, 0.0);
         for b in boids {
             if b.num != self.num {
                 if self.distance_from(b) <= self.view_distance as f64 {
@@ -72,7 +84,7 @@ impl Boid {
         out[0] *= -1.0;
         out[1] *= -1.0;
         if neighbour_count >= 0.0 {
-            return vm::vec2_normalized(out);
+            return na::base::Matrix::normalize(&out);
         } else {
             return out;
         }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,54 @@
+use alea::f32_less_than;
+use alea::f64_less_than;
+use alea::i32_less_than;
+
+use nalgebra::Vector2;
 use raylib::prelude::*;
 
+mod boid;
+
+use crate::boid::Boid;
+
+const BOID_COUNT: u16 = 128;
+const BOID_VIEW_DISTANCE: i32 = 12;
+const GRID_DIMENSIONS: [i8; 2] = [5, 5];
+const WINDOW_DIMENSIONS: [i32; 2] = [1280, 720];
+
+fn create_boids(number: u16) -> Vec<Boid> {
+    let mut out: Vec<Boid> = Vec::new();
+    for i in 0..number {
+        out.push(Boid {
+            colour: Color::new(
+                i32_less_than(255) as u8,
+                i32_less_than(255) as u8,
+                i32_less_than(255) as u8,
+                i32_less_than(255) as u8,
+            ),
+            position: [
+                f64_less_than(WINDOW_DIMENSIONS[0] as f64),
+                f64_less_than(WINDOW_DIMENSIONS[1] as f64),
+            ],
+            vector: Vector2::new(f64_less_than(1.0), f64_less_than(1.0)),
+            view_distance: BOID_VIEW_DISTANCE,
+            num: i as u8,
+        });
+    }
+    out
+}
+
 fn main() {
-    let (mut rl, thread) = raylib::init().size(1280, 720).title("hello world").build();
+    let (mut rl, thread) = raylib::init()
+        .size(WINDOW_DIMENSIONS[0], WINDOW_DIMENSIONS[1])
+        .title("hello world")
+        .build();
+    let mut boids = create_boids(BOID_COUNT);
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::DARKGRAY);
-        d.draw_text("Hello World!", 12, 12, 20, Color::WHITE);
+        d.clear_background(Color::new(127, 127, 127, 255));
+        for b in &boids {
+            b.render(&mut d, WINDOW_DIMENSIONS);
+        }
     }
 }
